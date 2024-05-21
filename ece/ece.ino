f092b5d2d8508869d5d7ef63fd512c214d1d99e6
AidanV <aidanvanduyne@gmail.com>
2024-05-21 15:14:02 -0700

    makes it to finish line

--- ece/ece.ino
+++ ece/ece.ino
@@ -1,14 +1,17 @@
 #include <ECE3.h>
 #include <stdio.h>
 
-#define RAMP_SPEED (0.00001)
+#define RAMP_SPEED (0.0005)
 #define NUM_PREVIOUS_ERRORS (3)
-const int left_nslp_pin=31; // nslp HIGH ==> awake & ready for PWM
-const int right_nslp_pin=11; // nslp HIGH ==> awake & ready for PWM
-const int left_dir_pin=29;
-const int right_dir_pin=30;
-const int left_pwm_pin=40;
-const int right_pwm_pin=39;
+#define NUM_PREVIOUS_SUMS (30)
+
+
+const int left_nslp_pin = 31; // nslp HIGH ==> awake & ready for PWM
+const int right_nslp_pin = 11; // nslp HIGH ==> awake & ready for PWM
+const int left_dir_pin = 29;
+const int right_dir_pin = 30;
+const int left_pwm_pin = 40;
+const int right_pwm_pin = 39;
 
 const int LED_RF = 75;
 int wheelSpd = 80;
@@ -17,9 +20,11 @@ int distance = 300;
 uint16_t sensorCalibrationMins[8] = {827, 617, 663, 640, 640, 594, 686, 709};
 uint16_t sensorCalibrationMaxs[8] = {2500, 2500, 2500, 2500, 2500, 2500, 2500, 2500};
 
-const float Kp = 4.0;
-const float Ki = 0.5;
-const float Kd = 2.0;
+const float Kp = 0.8;
+const float Ki = 0.0;
+const float Kd = 10.0;
+
+enum MovementState { LINE_FOLLOWING, TURNING_AROUND };
 
 typedef struct {
   float currentForwardSpeed = 0.0;
@@ -27,46 +32,48 @@ typedef struct {
   float currentError = 0.0;
   float previousErrors[NUM_PREVIOUS_ERRORS] = {0.0};
   uint16_t sensorValues[8] = {0};
+  int previousSums[NUM_PREVIOUS_SUMS] = {0};
+  MovementState movementState = LINE_FOLLOWING;
 } CarState;
 
-CarState * const state = new CarState();
+CarState state;
 
 
 void setup()
 {
   ECE3_Init();
-  
-  pinMode(left_nslp_pin,OUTPUT);
-  pinMode(left_dir_pin,OUTPUT);
-  pinMode(left_pwm_pin,OUTPUT);
-  pinMode(right_nslp_pin,OUTPUT);
-  pinMode(right_dir_pin,OUTPUT);
-  pinMode(right_pwm_pin,OUTPUT);
 
-//  pinMode(13,INPUT);
+  pinMode(left_nslp_pin, OUTPUT);
+  pinMode(left_dir_pin, OUTPUT);
+  pinMode(left_pwm_pin, OUTPUT);
+  pinMode(right_nslp_pin, OUTPUT);
+  pinMode(right_dir_pin, OUTPUT);
+  pinMode(right_pwm_pin, OUTPUT);
+
+  //  pinMode(13,INPUT);
 
   pinMode(LED_RF, OUTPUT);
 
-  digitalWrite(left_nslp_pin,HIGH);
-  digitalWrite(right_nslp_pin,HIGH);
-//  digitalWrite(left_nslp_pin,LOW);
-//  digitalWrite(right_nslp_pin,LOW);
-  
-// set the data rate in bits/second for serial data transmission
-  Serial.begin(9600); 
+  digitalWrite(left_nslp_pin, HIGH);
+  digitalWrite(right_nslp_pin, HIGH);
+  //  digitalWrite(left_nslp_pin,LOW);
+  //  digitalWrite(right_nslp_pin,LOW);
+
+  // set the data rate in bits/second for serial data transmission
+  Serial.begin(9600);
 
   resetEncoderCount_left();
   resetEncoderCount_right();
 
-  delay(2000); //Wait 2 seconds before starting 
-  
+  delay(2000); //Wait 2 seconds before starting
+
 }
 
 // speed is -1.0 to 1.0
 // turn clockwise is positive
 void move() {
-  float straightSpeed = state->currentForwardSpeed;
-  float turnSpeed = state->currentTurnSpeed;
+  float straightSpeed = state.currentForwardSpeed;
+  float turnSpeed = state.currentTurnSpeed;
 
   straightSpeed = max(-1.0, min(1.0, straightSpeed));
   turnSpeed = max(-1.0, min(1.0, turnSpeed));
@@ -74,23 +81,23 @@ void move() {
   float rightWheelSpeed = straightSpeed - 0.5 * turnSpeed;
   float leftWheelSpeed = straightSpeed + 0.5 * turnSpeed;
 
-  if(leftWheelSpeed < 0.0) {
+  if (leftWheelSpeed < 0.0) {
     digitalWrite(left_dir_pin, HIGH);
   } else {
     digitalWrite(left_dir_pin, LOW);
   }
   leftWheelSpeed = abs(leftWheelSpeed);
 
-  if(rightWheelSpeed < 0.0) {
+  if (rightWheelSpeed < 0.0) {
     digitalWrite(right_dir_pin, HIGH);
   } else {
     digitalWrite(right_dir_pin, LOW);
   }
   rightWheelSpeed = abs(rightWheelSpeed);
-  
-  analogWrite(left_pwm_pin, (int)(leftWheelSpeed * 255.0));    
-  analogWrite(right_pwm_pin, (int)(rightWheelSpeed * 255.0)); 
-  
+
+  analogWrite(left_pwm_pin, (int)(leftWheelSpeed * 255.0));
+  analogWrite(right_pwm_pin, (int)(rightWheelSpeed * 255.0));
+
 }
 
 
@@ -99,7 +106,7 @@ void move() {
 
 // increment is always positive
 void rampSpeedTo(float &currentSpeed, float toSpeed, float increment) {
-  if(toSpeed >= 0.0) {
+  if (toSpeed >= 0.0) {
     currentSpeed += increment;
     currentSpeed = min(currentSpeed, toSpeed);
   } else {
@@ -111,44 +118,114 @@ void rampSpeedTo(float &currentSpeed, float toSpeed, float increment) {
 float currentForwardSpeed = 0.0;
 float currentTurnSpeed = 0.0;
 
-float pid() {
-  return 0.0;
+void calcTurnSpeedPD() {
+  float p = state.currentError;
+  float d = 0.0;
+  d += state.currentError - state.previousErrors[0];
+  for (int i = 0; i < NUM_PREVIOUS_ERRORS - 1; i++) {
+    d += state.previousErrors[i] - state.previousErrors[i + 1];
+  }
+  d /= NUM_PREVIOUS_ERRORS;
+
+  state.currentTurnSpeed = p * Kp + d * Kd;
 }
 
-void updateErrors(){
-  int fusion[8] = {-4000, -3000, -2000, -1000, 1000, 2000, 3000, 4000};
-  int weightedsum = 0;
-  int totalsum = 0;
-  for(int i=0; i<8; i++){
-    weightedsum+=fusion[i]*state->sensorValues[i];
-    totalsum += state->sensorValues[i];
+void updateSensors() {
+  float fusion[8] = {1.0, 0.75, 0.5, 0.25, -0.25, -0.5, -0.75, -1.0};
+  float weightedsum = 0.0;
+  int sum = 0;
+  float totalsum = 0.0;
+  for (int i = 0; i < 8; i++) {
+    sum += state.sensorValues[i];
+    weightedsum += fusion[i] * state.sensorValues[i];
+    totalsum += state.sensorValues[i];
   }
-  float previousError = state->currentError;
-  if (totalsum == 0){
-    state->currentError = 0;
+
+  for (int i = 0; i < NUM_PREVIOUS_SUMS - 1; i++) {
+    state.previousSums[i+1] = state.previousSums[i];
   }
-  state->currentError = weightedsum/totalsum;
-  for (int i = 0; i < NUM_PREVIOUS_ERRORS - 2; i++){
-    state->previousErrors[i+1] = state->previousErrors[i]; 
+  state.previousSums[0] = sum;
+  
+  float previousError = state.currentError;
+  if (totalsum == 0) {
+    state.currentError = 0;
   }
-  state->previousErrors[0] = previousError;
+  state.currentError = weightedsum / totalsum;
+  for (int i = 0; i < NUM_PREVIOUS_ERRORS - 1; i++) {
+    state.previousErrors[i + 1] = state.previousErrors[i];
+  }
+  state.previousErrors[0] = previousError;
 }
 
-void normalize(){
-  for(int i=0; i<8; i++){
-    if (state->sensorValues[i] > sensorCalibrationMins[i]){
-      state->sensorValues[i] = (((state->sensorValues[i])-sensorCalibrationMins[i])*1000)/sensorCalibrationMaxs[i];
+void normalize() {
+  for (int i = 0; i < 8; i++) {
+    if (state.sensorValues[i] > sensorCalibrationMins[i]) {
+      state.sensorValues[i] = (((state.sensorValues[i]) - sensorCalibrationMins[i]) * 1000) / sensorCalibrationMaxs[i];
     } else {
-      state->sensorValues[i] = 0;
+      state.sensorValues[i] = 0;
     }
   }
 }
 
+bool onFinish() {
+  int avg = 0;
+  for (int i = 0; i < NUM_PREVIOUS_SUMS; i++) {
+     avg += state.previousSums[i];
+  }
+  avg /= NUM_PREVIOUS_SUMS;
+//  Serial.print(avg);
+//  Serial.print('\n');
+  return avg > 5800;
+}
+
+void stopMove() {
+  state.currentForwardSpeed = 0.0;
+  state.currentTurnSpeed = 0.0;
+  move();
+}
+
+void printError() {
+  Serial.print(state.currentError);
+  Serial.print('\n');
+}
+
+void printErrorChanges() {
+  Serial.print(state.currentError);
+  Serial.print('\t');
+  for (int i = 0; i < NUM_PREVIOUS_ERRORS; i++) {
+    Serial.print(state.previousErrors[i]);
+    Serial.print('\t');
+  }
+  Serial.print('\n');
+}
+
+void printTurnSpeed() {
+  Serial.print(state.currentTurnSpeed);
+  Serial.print('\n');
+}
 
 void loop() {
-  //ECE3_read_IR(state->sensorValues);
-  //normalize();
-  //updateErrors();
-  rampSpeedTo(state->currentForwardSpeed, 0.5, RAMP_SPEED);
+  ECE3_read_IR(state.sensorValues);
+  normalize();
+  updateSensors();
+
+  switch (state.movementState) {
+    case LINE_FOLLOWING:
+      if (onFinish()) {
+//        Serial.print("yes\n");
+        state.movementState = TURNING_AROUND;
+        stopMove();
+        return;
+      }
+
+  //  printErrorChanges();
+      calcTurnSpeedPD();
+  //  printTurnSpeed();
+      rampSpeedTo(state.currentForwardSpeed, 0.1, RAMP_SPEED);
+      break;
+    case TURNING_AROUND:
+      break;
+  }
+
   move();
 }
